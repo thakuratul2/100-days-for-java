5c3fa4dc56e18d606f4152723ddf80eadd9a65d9
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-19 14:17:04 +0530

    Day-1

--- day-1(Variables).java
+++ day-1(Variables).java
@@ -1,3 +1,36 @@
+//day-1.1 (Variables)
 class VariablesCode{
-    
+    public static void main(String[] args) {
+        // all data types
+        int a = 10;
+        int b = 20;
+        float c = 3.15f;
+        float d = 25.6f;
+        boolean e = true;
+        boolean f = false;
+        String g = "Hello World";
+        String h = "Hello World2";
+        long i = 1234567890123456789L;
+        long j = 12345689L;
+        short k = 123;
+        short l = 1234;
+        char m = 'a';
+        char n = 'b';
+
+        //print all the variables
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(m);
+        System.out.println(n);
+    }
 }
