6b93e2de7d0e8aacf55863b205fe4e587550f3ae
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-19 20:57:31 +0530

    Day-1

---
+++ day-1(comparison).java
@@ -0,0 +1,21 @@
+class Comparison {
+    public static void main(String[] args) {
+        // Comparison Operators
+        int a = 10;
+        int b = 20;
+        
+        //equal to
+        System.out.println("a == b : " + (a == b));
+        //not equal to
+        System.out.println("a != b : " + (a != b));
+        //greater than
+        System.out.println("a > b : " + (a > b));
+        //less than
+        System.out.println("a < b : " + (a < b));
+        //greater than or equal to
+        System.out.println("a >= b : " + (a >= b));
+        //less than or equal to
+        System.out.println("a <= b : " + (a <= b));
+    }
+    
+}
