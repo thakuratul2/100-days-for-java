8fd7d6944adf4a23f8be8c9f093550e4eb42cfd8
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-21 13:48:36 +0530

    Day-2

---
+++ day-2(else-if).java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Elseif {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Marks :");
+        int a = sc.nextInt();
+        if(a>=90){
+            System.out.println("Grade A");
+        }else if(a>=85 && a<90){
+            System.out.println("Garde B");
+        }else if(a>=75 && a<85){
+            System.out.println("Garde C");
+        }else{
+            System.out.println("Fail");
+        }
+    }
+}
