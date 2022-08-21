3caa30b7758894281e7cadfb646419901d2cf192
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-21 21:13:03 +0530

    Day-2

--- day-2(Loops).java
+++ day-2(Loops).java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 class Loops {
-    
+    //While Loop
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number: ");
+        int a = sc.nextInt();
+        while(a<=25){
+            System.out.println(a);
+            a++;
+        }
+        //For Loop
+
+    }
 }
