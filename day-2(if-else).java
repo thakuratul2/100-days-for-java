55c73f2976ed041c92f78fdeb8524f7ab6136f5c
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-21 13:49:18 +0530

    day-2

---
+++ day-2(if-else).java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Ifelse{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b){
+            System.out.println(a+" "+"is greater than"+" "+b);
+        }else{
+            System.out.println(a+" "+"is less than"+" "+b);
+        }
+        }
+    }
+
