9910b9c5fb7ac60e8cf6fc320e781a9a9bb0e2ad
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-25 13:07:12 +0530

    Day-3

---
+++ day-3(switch case).java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Switch{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);{
+            System.out.println("Enter the Day Number: ");
+            int day = sc.nextInt();
+            //switch case
+
+            switch(day){
+                case 1:
+                  System.out.println("Monday");
+                    break;
+                case 2:
+                   System.out.println("Tuesday");
+                    break;
+                case 3:
+                     System.out.println("Wednesday");
+                      break;
+                case 4:
+                    System.out.println("Thursday");
+                     break;
+                case 5:
+                System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                default:
+                 System.out.println("Sunday");
+                    break;
+            }
+        }
+    }
+}
