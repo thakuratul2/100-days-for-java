a22d5d7a19bb3e827822e1c62c99fafc6283baa4
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-26 20:18:00 +0530

    Day-3

--- day-3(Arrays).java
+++ day-3(Arrays).java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Arrays{
     public static void main(String[] args) {
         //arrays
@@ -27,5 +29,29 @@ class Arrays{
         String cars[] = {"BMW","Audi","Mercedes","Volvo"};
         cars[0] = "ATUL";
         System.out.println(cars[0]);
+
+        //2-D Arrays
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row number: ");
+        int row = sc.nextInt();
+        System.out.println("Enter the column number: ");
+        int col = sc.nextInt();
+        int[][] arr = new int[row][col];
+
+        //input the values in the array
+        for(int i = 0;i<row;i++){
+            for(int j = 0;j<col;j++){
+                arr[i][j] = sc.nextInt();
+                
+            }
+        }
+        //print the array
+        for(int i=0;i<row;i++){
+            for(int j =0;j<col;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
     }
 }
