3ebf9d077c8b48e76ce65e604d5be58237caa059
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-30 22:12:19 +0530

    Day-5

---
+++ Strings/day-5(charAt).java
@@ -0,0 +1,11 @@
+package Strings;
+
+class charAtuses {
+    public static void main(String[] args) {
+        //charAt
+        String name = "Shahid";
+        for(int i =0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
