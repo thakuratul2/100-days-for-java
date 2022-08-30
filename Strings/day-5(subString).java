467377cf3144df34dd20eaf0eda78bf89836f307
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-08-31 00:08:42 +0530

    Day-5

---
+++ Strings/day-5(subString).java
@@ -0,0 +1,10 @@
+package Strings;
+
+class subString {
+    public static void main(String[] args) {
+        //subString Functions
+        String name = "Atul Pratap Singh";
+        //subString(starting,ending)
+        System.out.println(name.substring(0,5));
+    }    
+}
