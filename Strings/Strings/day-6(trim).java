d4b0488f6b05ed154ae18f759bd09f01ba3e575e
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-01 13:18:35 +0530

    Day-6

---
+++ Strings/Strings/day-6(trim).java
@@ -0,0 +1,10 @@
+package Strings;
+
+class Trims{
+    public static void main(String[] args) {
+        String s = " Hello World ";
+        //it removes the white spaces from the beginning and end of the string
+        System.out.println(s.trim());
+        
+    }
+}
---
+++ Strings/Strings/day-6(upper&lower).java
@@ -0,0 +1,10 @@
+package Strings;
+
+class UpperLower {
+    public static void main(String[] args) {
+        //upper case & lower case
+        String sb = "Hello World";
+        System.out.println(sb.toUpperCase());
+        System.out.println(sb.toLowerCase());
+    }
+}
