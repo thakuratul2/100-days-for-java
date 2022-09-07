bbe8ced75fedd612e3002bccd4544c0b568b1269
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-07 23:21:04 +0530

    Day-8

--- Recursion/day-7(Recursion).java
+++ Recursion/day-7(Recursion).java
@@ -1,7 +1,27 @@
 package Recursion;
 
+import java.util.Scanner;
+
 class Recursion {
+    //recursive function
+    public static void Print(int n){
+        if(n==0){
+            return;
+        }
+
+        System.out.println(n);
+        Print(n-1);
+    }
+   
     public static void main(String[] args) {
-        
+        //print number from 5 to 1
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        Print(num);
+        System.out.println();
+        sc.close();
+
     }
 }
