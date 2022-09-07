44d6701de4eb53fd609733f56cede40ce8354891
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-09-07 23:25:18 +0530

    Day-8

--- Recursion/day-7(Recursion).java
+++ Recursion/day-7(Recursion).java
@@ -6,11 +6,11 @@ class Recursion {
     //recursive function
     public static void Print(int n){
         if(n==0){
-            return;
+            return;//base condition
         }
 
-        System.out.println(n);
-        Print(n-1);
+        System.out.println(n);//print statement
+        Print(n-1);//function call
     }
    
     public static void main(String[] args) {
