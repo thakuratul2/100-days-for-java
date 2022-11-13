d49bd26e254b884cbcd11a581565b3ee5febcbe6
thakuratul2 <pratapsinghatul111@gmail.com>
2022-11-13 22:03:39 +0530

    Day-23

--- Oops/day-23(Encapsulation).java
+++ Oops/day-23(Encapsulation).java
@@ -1,6 +1,23 @@
+class Animal2{
+    private String name;
+
+    //getters
+    public String SetWork(){
+        return this.name;
+    }
+
+    //setters
+    public void getWork(String name){
+      this.name= name;
+    }
+}
+
 
 class Encapsulation {
     public static void main(String[] args) {
-        System.out.println("Hello World2");
+        //Encapsulation-> Defines to make sure that 'sensitive' data is hidden from users.
+        //declare class as private to use this.
+        //to access it use setters and getters
+        
     }
 }
