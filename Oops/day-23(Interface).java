069fc3dd0c8069a2cfbe550e2e6e46a9b6822765
thakuratul2 <pratapsinghatul111@gmail.com>
2022-11-14 12:22:32 +0530

    Day-23

--- Oops/day-23(Interface).java
+++ Oops/day-23(Interface).java
@@ -1,5 +1,19 @@
+interface Cars{
+    void Drive();
+}
+class BMWBrand implements Cars{
+       public void Drive(){
+        System.out.println("Cars speed was 40KM");
+       }
+}
 class Interface {
     public static void main(String[] args) {
-        
+
+        //interface:- it is a completely "abstract class" that is used to group related method with empty bodies;
+        //Constructors does not allow in the interface
+        BMWBrand h1 = new BMWBrand();
+
+        h1.Drive();
+
     }
 }
